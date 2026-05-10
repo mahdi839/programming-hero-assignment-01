41fb805518bf1c528c9ad962bfaa5895c511d5a5
Mehedi Hasan <hasanarefi56574@gmail.com>
2026-05-10 12:41:46 +0600

    solved assignment 7

--- src/solutions.ts
+++ src/solutions.ts
@@ -1,13 +1,13 @@
 // problem 01
-function filterEvenNumbers (numbers:number[]):number[]{
-    return numbers.filter((number)=>number %2===0)
+function filterEvenNumbers(numbers: number[]): number[] {
+    return numbers.filter((number) => number % 2 === 0)
 }
 
-console.log(filterEvenNumbers([2,4,7,9]))
+console.log(filterEvenNumbers([2, 4, 7, 9]))
 
 // problem 02
 
-function reverseString (revStr:string):string{
+function reverseString(revStr: string): string {
     return revStr.split("").reverse().join("");
 }
 
@@ -16,10 +16,10 @@ console.log(reverseString('tpircsepyt'))
 // problem 03
 type StringOrNumber = string | number;
 
-function checkType (value:StringOrNumber):string{
-    if(typeof value==='string'){
+function checkType(value: StringOrNumber): string {
+    if (typeof value === 'string') {
         return 'String';
-    }else{
+    } else {
         return 'Number';
     }
 }
@@ -27,15 +27,15 @@ function checkType (value:StringOrNumber):string{
 console.log(checkType('hello'))
 
 // problem 04
- 
-function getProperty<t,k extends keyof t> (obj:t,key:k):t[k]{
-  return obj[key];
+
+function getProperty<t, k extends keyof t>(obj: t, key: k): t[k] {
+    return obj[key];
 }
 
 const user = {
-  id: 1,
-  name: "John Doe",
-  age: 21,
+    id: 1,
+    name: "John Doe",
+    age: 21,
 };
 
 const result = getProperty(user, "name");
@@ -45,25 +45,25 @@ console.log(result);
 // problem 05
 
 type Book = {
-  title: string;
-  author: string;
-  publishedYear: number;
+    title: string;
+    author: string;
+    publishedYear: number;
 };
 
 function toggleReadStatus<T extends Book>(
-  book: T
+    book: T
 ): T & { isRead: boolean } {
-  return {
-    ...book,
-    isRead: true,
-  };
+    return {
+        ...book,
+        isRead: true,
+    };
 }
 
 
 const myBook = {
-  title: "TypeScript Guide",
-  author: "Jane Doe",
-  publishedYear: 2024,
+    title: "TypeScript Guide",
+    author: "Jane Doe",
+    publishedYear: 2024,
 };
 
 const updatedBook = toggleReadStatus(myBook);
@@ -71,6 +71,45 @@ const updatedBook = toggleReadStatus(myBook);
 console.log(updatedBook);
 
 // problem 06
+
+class Person {
+    name: string;
+    age: number
+    constructor(name: string, age: number) {
+      this.name = name;
+      this.age = age;
+    }
+}
+
+class Student extends Person{
+   grade:string;
+   constructor(name: string, age: number,grade:string){
+     super(name,age);
+     this.grade = grade;
+   }
+
+   getDetails ():string{
+     return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+   }
+}
+
+const student = new Student("Alice", 20, "A");
+
+console.log(student.getDetails());
+
+
+// problem 07
+
+function getIntersection (array1:number[],array2:number[]){
+    return array1.filter((item)=>array2.includes(item))
+}
+
+const resultOfTheProblem = getIntersection(
+  [1, 2, 3, 4, 5],
+  [3, 4, 5, 6, 7]
+);
+
+console.log(resultOfTheProblem);
 
 
 
